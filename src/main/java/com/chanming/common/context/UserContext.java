2c8fdeeb08c12b73a86b8f116df72b82e39398b7
chanming.cm <chanming.cm@alibaba-inc.com>
2016-07-29 10:33:10 +0800

    0.0.4

---
+++ src/main/java/com/chanming/common/context/UserContext.java
@@ -0,0 +1,15 @@
+package com.chanming.common.context;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by chanming on 16/7/29.
+ */
+
+public class UserContext {
+    /**
+     * ÓÎÏ·×´Ì¬
+     */
+    private @Getter @Setter int gameStatus;
+}
